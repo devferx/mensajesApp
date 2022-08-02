f6593c5e510e037e18329b3c44cd7b51161f0c27
devferx <ferkki@hotmail.com>
2022-08-02 15:35:15 -0400

    Pantalla de selección de Chat

--- src/pages/ChatPage.tsx
+++ src/pages/ChatPage.tsx
@@ -2,18 +2,14 @@ import { InboxPeople } from "../components/InboxPeople";
 import { Messages } from "../components/Messages";
 
 import "../css/chat.css";
+import { ChatSelect } from "../components/ChatSelect";
 
 export const ChatPage = () => {
   return (
     <div className="messaging">
       <div className="inbox_msg">
-        {/* <!-- Inbox people inicio --> */}
         <InboxPeople />
-        {/* <!-- Inbox people Fin --> */}
-
-        {/* <!-- Chat inicio --> */}
-        <Messages />
-        {/* <!-- Chat Fin --> */}
+        {true ? <Messages /> : <ChatSelect />}
       </div>
     </div>
   );
