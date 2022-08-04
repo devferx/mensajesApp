5f678c9ab9cf61b97d08131c3513bf17df4a9b1e
devferx <ferkki@hotmail.com>
2022-08-04 11:12:13 -0400

    :lipstick: [ADD] URL

--- src/helpers/fetch.ts
+++ src/helpers/fetch.ts
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-export const baseURL = import.meta.env.VITE_BASE_URL || "localhost:3001/api";
+export const baseURL =
+  import.meta.env.VITE_BASE_URL || "http://localhost:3001/api";
 
 export const fetchWithoutToken = axios.create({
   baseURL,
