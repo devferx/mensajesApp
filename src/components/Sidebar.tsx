10d61b22a53c3bf32b5c9fb5d226762fcf163e8c
devferx <ferkki@hotmail.com>
2022-11-12 09:42:32 -0400

    Mostrar listado de los usuarios

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,11 +1,20 @@
+import { useContext } from "react";
+
 import { SidebarChatItem } from "./SidebarChatItem";
+import { ChatContext } from "../context/chat/ChatContext";
+import { AuthContext } from "../auth/AuthContext";
+
 export const Sidebar = () => {
-  const chats = new Array(20).fill(0);
+  const { chatState } = useContext(ChatContext);
+  const { auth } = useContext(AuthContext);
+
   return (
     <div className="inbox_chat">
-      {chats.map((chat, idx) => (
-        <SidebarChatItem key={idx} />
-      ))}
+      {chatState.users
+        .filter((user) => user.uid != auth.uid)
+        .map((user) => (
+          <SidebarChatItem key={user.uid} user={user} />
+        ))}
 
       {/* <!-- Espacio extra para scroll --> */}
       <div className="extra_space"></div>
--- src/components/SidebarChatItem.tsx
+++ src/components/SidebarChatItem.tsx
@@ -1,4 +1,10 @@
-export const SidebarChatItem = () => {
+import type { User } from "../interfaces";
+
+interface SidebarChatItemProps {
+  user: User;
+}
+
+export const SidebarChatItem = ({ user }: SidebarChatItemProps) => {
   return (
     <div className="chat_list">
       {/* active_chat */}
@@ -10,9 +16,12 @@ export const SidebarChatItem = () => {
           />
         </div>
         <div className="chat_ib">
-          <h5>Some random name</h5>
-          <span className="text-success">Online</span>
-          <span className="text-danger">Offline</span>
+          <h5>{user.name}</h5>
+          {user.online ? (
+            <span className="text-success">Online</span>
+          ) : (
+            <span className="text-danger">Offline</span>
+          )}
         </div>
       </div>
     </div>
